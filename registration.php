8767bae638a30ae31982aba84523918d7b9fb4dd
ASHOK RAMJI SHANMUGAM <32545963+ashokramji@users.noreply.github.com>
2020-03-12 16:23:07 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -2,4 +2,4 @@
 
 use \Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Nwdthemes_ArraySerialized', __DIR__);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Ramji_Sample', __DIR__);
